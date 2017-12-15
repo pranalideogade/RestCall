b15f27d7e5aa524685c640827c3b222ef1925027
pranali_deogade <pranali_deogade@persistent.co.in>
2017-12-15 18:46:56 +0530

    pran

---
+++ app/src/main/java/com/example/pranali_deogade/mplib/Greeting.java
@@ -0,0 +1,31 @@
+package com.example.pranali_deogade.mplib;
+
+/**
+ * Created by pranali_deogade on 07-12-2017.
+ */
+
+public class Greeting {
+
+  /**  private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+   **/
+
+  private String status;
+  private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
